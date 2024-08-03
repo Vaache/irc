f206697792b085bc7c5302d999531503325cb8c4
Vache Hovhannisyan <hovhnanisyan@mail.ru>
2024-08-03 20:12:16 +0400

    [42Yerevan-500:patch] started and modified server

--- src/irc.cpp
+++ src/irc.cpp
@@ -1,20 +1,58 @@
 #include "ircserv.hpp"
+#include <iostream>
+#include <ostream>
 
 int main(int ac, char ** av) {
-    (void)ac;
-    (void)av;
-    std::string str = "Hello World";
-    std::cout << str << std::endl;
-	std::vector<int> arr;
+	if (ac != 3) {
+		std::cout << "Please enter the correct port and password." << std::endl;
+		return 1;
+	}
+	const int lvl = 1;
+	String sport = av[1];
+	String passwd = av[2];
+	int port = atoi(sport.c_str());
+	int clientSock;
+	int servSock = socket(AF_INET, SOCK_STREAM, 0);
 
-	
-	for (int i = 0; i < 100; ++i) {
-		arr.push_back(i);
+	if (servSock < 0) {
+		std::cerr << "servSock" << strerror(errno) << std::endl;
+		return 1;
 	}
 
-	for (unsigned long i = 0; i < arr.size(); ++ i) {
-		std::cout << arr.at(i) << " ";
+	if (setsockopt(servSock, SOL_SOCKET, SO_REUSEADDR, (void *)&lvl, sizeof(lvl)) < 0) {
+		std::cerr << "setsockopt" << strerror(errno) << std::endl;
 	}
-	std::cout << std::endl;
+
+	struct sockaddr_in in, client_in;
+	socklen_t clientLen = sizeof(client_in);
+
+	memset((void*)&in, 0, sizeof(in));
+	in.sin_family = AF_INET;
+	in.sin_addr.s_addr = INADDR_ANY;
+	in.sin_port = htons(port);
+
+
+	if (bind(servSock, (struct sockaddr *)&in, sizeof(in)) == -1) {
+		std::cerr << "bind" << strerror(errno) << std::endl;
+		return 1;
+	}
+
+    if (listen(servSock, 10) == -1) {
+		std::cerr << "listen" << strerror(errno) << std::endl;
+        return 1;
+	}
+
+	while (1) {
+		clientSock = accept(servSock, (struct sockaddr *)&client_in, &clientLen);
+		if (clientSock < 0) {
+			std::cerr << "accept" << strerror(errno) << std::endl;
+			continue;
+		}
+		char buff[1024] = {0};
+
+		while (recv(clientSock, buff, sizeof(buff), 0) != -1)
+			std::cout << buff;
+	}
+
 	return 0;
 }
